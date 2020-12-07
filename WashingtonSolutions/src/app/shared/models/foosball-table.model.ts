2e3f1c230446f1f758e83b4884640826028fe42e
bram <bram.vermeulen69@gmail.com>
2020-12-07 15:53:00 +0100

    models

---
+++ WashingtonSolutions/src/app/shared/models/foosball-table.model.ts
@@ -0,0 +1,5 @@
+export class FoosballTable {
+  constructor(public foosballTableID: number,public tableName: string, public companyName: string, public address: string, public contactPerson: number, public tablePicture?: string) {
+
+  }
+}
