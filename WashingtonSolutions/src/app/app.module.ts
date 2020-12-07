cb84cdb8c0f5822e98af10cf7270c2532306533c
bram <bram.vermeulen69@gmail.com>
2020-12-07 13:41:31 +0100

    structuur

--- WashingtonSolutions/src/app/app.module.ts
+++ WashingtonSolutions/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Error404Component } from './shared/components/error/error404/error404.component';
+import { Error403Component } from './shared/components/error/error403/error403.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Error404Component,
+    Error403Component
   ],
   imports: [
     BrowserModule,
