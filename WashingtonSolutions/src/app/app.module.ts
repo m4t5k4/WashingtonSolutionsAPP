0ade540823e78d0e202e7ef82ad93bc4372d9788
Matthias Reynders <matthias.reynders.1@student.kdg.be>
2020-12-07 16:01:39 +0100

    jwt interceptor

--- WashingtonSolutions/src/app/app.module.ts
+++ WashingtonSolutions/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { Error403Component } from './shared/components/error/error403/error403.c
 import { AlertComponent } from './shared/components/alert/alert.component';
 import { LoginComponent } from './modules/home/login/login.component';
 import { SharedModule } from './shared/shared.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,7 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     SharedModule
   ],
-  providers: [],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
