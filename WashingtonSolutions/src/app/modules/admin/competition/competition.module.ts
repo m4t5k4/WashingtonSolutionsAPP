141e404bcd75b0c54f3fd6d83f4db095ec6c17b1
Seppe Alaerts <r0749143@student.thomasmore.be>
2020-12-08 14:03:26 +0100

    competition structure

--- WashingtonSolutions/src/app/modules/admin/competition/competition.module.ts
+++ WashingtonSolutions/src/app/modules/admin/competition/competition.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ManageCompetitionComponent } from './manage-competition/manage-competition.component';
+import { AddCompetitionComponent } from './add-competition/add-competition.component';
+import { EditCompetitionComponent } from './edit-competition/edit-competition.component';
+import { SharedModule } from '../../../shared/shared.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [ManageCompetitionComponent, AddCompetitionComponent, EditCompetitionComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+  ],
+  exports: [
+    ManageCompetitionComponent, 
+    AddCompetitionComponent, 
+    EditCompetitionComponent,
   ]
 })
+
 export class CompetitionModule { }
---
+++ WashingtonSolutions/src/app/modules/admin/competition/manage-competition/manage-competition.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-competition',
+  templateUrl: './manage-competition.component.html',
+  styleUrls: ['./manage-competition.component.scss']
+})
+export class ManageCompetitionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
