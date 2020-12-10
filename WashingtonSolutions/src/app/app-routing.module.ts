52e5440722d324eb38d5bdde14f9bbeaabc86d7e
Seppe Alaerts <r0749143@student.thomasmore.be>
2020-12-10 09:53:53 +0100

    Manage competitions

--- WashingtonSolutions/src/app/app-routing.module.ts
+++ WashingtonSolutions/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { HomepageComponent } from './modules/home/homepage/homepage.component';
 import { Error404Component } from './shared/components/error/error404/error404.component';
 import { LoginComponent } from './modules/home/login/login.component';
 import { ManageCompetitionComponent } from './modules/admin/competition/manage-competition/manage-competition.component';
+import { AddCompetitionComponent } from './modules/admin/competition/add-competition/add-competition.component';
+import { EditCompetitionComponent } from './modules/admin/competition/edit-competition/edit-competition.component';
 import { RegisterComponent } from './modules/home/register/register.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'login', component: LoginComponent},
   { path: 'competition', component: ManageCompetitionComponent},
+  { path: 'add-competition', component: AddCompetitionComponent},
+  { path: 'edit-competition', component: EditCompetitionComponent},
   { path: 'register', component: RegisterComponent},
   { path: '**', component: Error404Component }
 ];
