915dab0cbd544f4bcc72f2db6e7bb1d3145842ab
Seppe Alaerts <r0749143@student.thomasmore.be>
2020-12-12 20:03:54 +0100

    game details

--- WashingtonSolutions/src/app/modules/user/games/game.module.ts
+++ WashingtonSolutions/src/app/modules/user/games/game.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameComponent } from './game.component';
+import { UserGameDetailComponent } from './details/user-game-detail/user-game-detail.component';
 
 
 @NgModule({
-  declarations: [GameComponent],
+  declarations: [GameComponent, UserGameDetailComponent],
   imports: [
     CommonModule
   ]
